2c3c33efbbcca5799ad54c72f3c50add50b0b91a
Jefferson Huera <jefferh30@gmail.com>
2023-03-01 13:42:11 -0300

    Add JS file that holds the questions, options and answers

---
+++ questions.js
@@ -0,0 +1,62 @@
+//QUESTIONS
+// creating an array and passing the number, questions, options, and answers
+//
+const questions = [
+    {
+    numb: 1,
+    question: "Cuánto es 2+3?",
+    answer: "A",
+    options: [
+      `<span class="letterQuiz">A</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">B</span> $$3x+2=4$$`,
+      `<span class="letterQuiz">C</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">D</span> $latex 2x+2=5$`
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Cuánto es 2+3?",
+    answer: "A",
+    options: [
+      `<span class="letterQuiz">A</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">B</span> $$3x+2=4$$`,
+      `<span class="letterQuiz">C</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">D</span> $latex 2x+2=5$`
+    ]
+  },
+  {
+    numb: 3,
+    question: "Cuánto es 2+3?",
+    answer: "A",
+    options: [
+      `<span class="letterQuiz">A</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">B</span> $$3x+2=4$$`,
+      `<span class="letterQuiz">C</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">D</span> $latex 2x+2=5$`
+    ]
+  },
+  {
+    numb: 4,
+    question: "Cuánto es 2+3?",
+    answer: "B",
+    options: [
+      `<span class="letterQuiz">A</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">B</span> $$3x+2=4$$`,
+      `<span class="letterQuiz">C</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">D</span> $latex 2x+2=5$`
+    ]
+  },
+  {
+    numb: 5,
+    question: "Cuánto es 2+3?",
+    answer: "D",
+    options: [
+      `<span class="letterQuiz">A</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">B</span> $$3x+2=4$$`,
+      `<span class="letterQuiz">C</span> $latex 2x+2=5$`,
+      `<span class="letterQuiz">D</span> $latex 2x+2=5$`
+    ]
+  },
+
+];
